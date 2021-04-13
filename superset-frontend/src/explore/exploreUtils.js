0da822f4a8fb247be72e9e31f3f4df16b12f92b7
Aymaru <aycastillo@ic-itcr.ac.cr>
2021-04-13 11:02:44 -0600

    added .xlsx endpoint resolver

--- superset-frontend/src/explore/exploreUtils.js
+++ superset-frontend/src/explore/exploreUtils.js
@@ -73,7 +73,7 @@ export function getAnnotationJsonUrl(slice_id, form_data, isNative) {
 export function getURIDirectory(endpointType = 'base') {
   // Building the directory part of the URI
   if (
-    ['full', 'json', 'csv', 'query', 'results', 'samples'].includes(
+    ['full', 'json', 'csv', 'query', 'results', 'samples','xlsx'].includes(
       endpointType,
     )
   ) {
@@ -172,6 +172,9 @@ export function getExploreUrl({
   if (endpointType === 'csv') {
     search.csv = 'true';
   }
+  if (endpointType === 'xlsx') {
+    search.xlsx = 'true';
+  }
   if (endpointType === 'standalone') {
     search.standalone = 'true';
   }
@@ -223,7 +226,7 @@ export const buildV1ChartDataPayload = ({
 };
 
 export const getLegacyEndpointType = ({ resultType, resultFormat }) =>
-  resultFormat === 'csv' ? resultFormat : resultType;
+  ['csv','xlsx'].includes(resultFormat) ? resultFormat : resultType;
 
 export function postForm(url, payload, target = '_blank') {
   if (!url) {
@@ -259,7 +262,7 @@ export const exportChart = ({
   let url;
   let payload;
   if (shouldUseLegacyApi(formData)) {
-    const endpointType = getLegacyEndpointType({ resultFormat, resultType });
+    const endpointType = getLegacyEndpointType({ resultFormat, resultType });    
     url = getExploreUrl({
       formData,
       endpointType,
