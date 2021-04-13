e3d0d5a30114de6de6096ef7ff246a44fc7864f5
Aymaru <aycastillo@ic-itcr.ac.cr>
2021-04-13 11:00:29 -0600

    added xlswriter dependecy

--- setup.py
+++ setup.py
@@ -110,6 +110,7 @@ setup(
         "wtforms-json",
         "pyparsing>=2.4.7, <3.0.0",
         "holidays==0.10.3",  # PINNED! https://github.com/dr-prodigy/python-holidays/issues/406
+        "xlsxwriter==1.3.8"
     ],
     extras_require={
         "athena": ["pyathena>=1.10.8,<1.11"],
