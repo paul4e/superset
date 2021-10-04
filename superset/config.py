a9ebcb879d3699df16f730b886db506aca6d16e9
Aymaru <aycastillo@ic-itcr.ac.cr>
2021-10-04 14:36:13 -0600

    changed feature flag for active reports

--- superset/config.py
+++ superset/config.py
@@ -400,7 +400,7 @@ DEFAULT_FEATURE_FLAGS: Dict[str, bool] = {
     # This could cause the server to run out of memory or compute.
     "ALLOW_FULL_CSV_EXPORT": False,
     "UX_BETA": False,
-    "ACTIVE_REPORTS": True,
+    "ACTIVE_REPORTS_JS": True,
 }
 
 # Feature flags may also be set via 'SUPERSET_FEATURE_' prefixed environment vars.
