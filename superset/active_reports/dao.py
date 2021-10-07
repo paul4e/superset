94dcf53d038c9d7b819660bc6a6d8e81bcd7c425
Aymaru <aycastillo@ic-itcr.ac.cr>
2021-10-07 15:46:13 -0600

    Updated dao - Active Reports

--- superset/active_reports/dao.py
+++ superset/active_reports/dao.py
@@ -24,6 +24,13 @@ from sqlalchemy.exc import SQLAlchemyError
 from superset.dao.base import BaseDAO
 from superset.extensions import db
 from superset.models.active_reports import ActiveReport
+from superset.active_reports.commands.exceptions import ActiveReportNotFoundError
+from superset.models.slice import Slice
+
+from superset import security_manager
+
+from superset.active_reports.filters import ActiveReportAccessFilter
+from superset.charts.commands.exceptions import ChartNotFoundError
 
 if TYPE_CHECKING:
     from superset.connectors.base.models import BaseDatasource
@@ -33,6 +40,37 @@ logger = logging.getLogger(__name__)
 
 class ActiveReportsDAO(BaseDAO):
     model_cls = ActiveReport
-    base_filter = None
+    base_filter = ActiveReportAccessFilter
 
     # include_route_methods
+    @staticmethod
+    def update_charts_owners(model: ActiveReport, commit: bool = True) -> ActiveReport:
+        owners = list(model.owners)
+        for slc in model.slices:
+            slc.owners = list(set(owners) | set(slc.owners))
+        if commit:
+            db.session.commit()
+        return model
+
+    @staticmethod
+    def get_by_id(report_id: str) -> ActiveReport:
+        report = db.session.query(ActiveReport).filter(ActiveReport.id == report_id).one_or_none()
+        if not report:
+            raise ActiveReportNotFoundError()
+        # security_manager.raise_for_dashboard_access(dashboard)
+        return report
+
+    @staticmethod
+    def get_charts_for_report(report_id: str) -> List[Slice]:
+        try:
+            slices = ActiveReportsDAO.get_by_id(report_id).slices
+        except Exception as e:
+            return e
+        return slices
+
+    @staticmethod
+    def update_charts_for_report(slices_id: List[int]) -> List[Slice]:
+        slices = db.session.query(Slice).filter(Slice.id.in_(slices_id)).all()
+        if not slices:
+            raise ChartNotFoundError()
+        return slices
