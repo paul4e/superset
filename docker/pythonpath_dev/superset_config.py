61d9adefa084f4af7d858536c7d6480ceb8ec0af
Aymaru <aycastillo@ic-itcr.ac.cr>
2021-11-04 14:56:39 -0600

    updated dockerfile and config for Alerts&Reports

--- docker/pythonpath_dev/superset_config.py
+++ docker/pythonpath_dev/superset_config.py
@@ -28,6 +28,13 @@ from typing import Optional
 from cachelib.file import FileSystemCache
 from celery.schedules import crontab
 
+from superset_config import *
+from cachelib import RedisCache
+from superset.typing import CacheConfig
+
+from flask_appbuilder.security.manager import AUTH_DB, AUTH_LDAP
+from superset.security.custom_manager import CDASecurityManager
+
 logger = logging.getLogger()
 
 
@@ -69,28 +76,62 @@ REDIS_RESULTS_DB = get_env_variable("REDIS_RESULTS_DB", "1")
 
 RESULTS_BACKEND = FileSystemCache("/app/superset_home/sqllab")
 
+THUMBNAIL_SELENIUM_USER = "admin"
+THUMBNAIL_CACHE_CONFIG: CacheConfig = {
+    'CACHE_TYPE': 'redis',
+    'CACHE_DEFAULT_TIMEOUT': 24 * 60 * 60 * 30,
+    'CACHE_KEY_PREFIX': 'thumbnail_',
+    'CACHE_NO_NULL_WARNING': True,
+    'CACHE_REDIS_URL': 'redis://%s:%s/1' % (REDIS_HOST, REDIS_PORT)
+}
+SCREENSHOT_LOCATE_WAIT = 100
+SCREENSHOT_LOAD_WAIT = 600
+RESULTS_BACKEND = RedisCache(host=REDIS_HOST, port=REDIS_PORT,
+                             key_prefix='superset_results')
+
 
 class CeleryConfig(object):
-    BROKER_URL = f"redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_CELERY_DB}"
-    CELERY_IMPORTS = ("superset.sql_lab", "superset.tasks")
-    CELERY_RESULT_BACKEND = f"redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_RESULTS_DB}"
-    CELERYD_LOG_LEVEL = "DEBUG"
-    CELERYD_PREFETCH_MULTIPLIER = 1
-    CELERY_ACKS_LATE = False
-    CELERYBEAT_SCHEDULE = {
-        "reports.scheduler": {
-            "task": "reports.scheduler",
-            "schedule": crontab(minute="*", hour="*"),
+    BROKER_URL = 'redis://%s:%s/0' % (REDIS_HOST, REDIS_PORT)
+    CELERY_IMPORTS = (
+    'superset.sql_lab', "superset.tasks", "superset.tasks.thumbnails",)
+    CELERY_RESULT_BACKEND = 'redis://%s:%s/0' % (REDIS_HOST, REDIS_PORT)
+    CELERYD_PREFETCH_MULTIPLIER = 10
+    CELERY_ACKS_LATE = True
+    CELERY_ANNOTATIONS = {
+        'sql_lab.get_sql_results': {
+            'rate_limit': '100/s',
         },
-        "reports.prune_log": {
-            "task": "reports.prune_log",
-            "schedule": crontab(minute=10, hour=0),
+        'email_reports.send': {
+            'rate_limit': '1/s',
+            'time_limit': 600,
+            'soft_time_limit': 600,
+            'ignore_result': True,
+        },
+    }
+    CELERYBEAT_SCHEDULE = {
+        'reports.scheduler': {
+            'task': 'reports.scheduler',
+            'schedule': crontab(minute='*', hour='*'),
+        },
+        'reports.prune_log': {
+            'task': 'reports.prune_log',
+            'schedule': crontab(minute=0, hour=0),
+        },
+        'cache-warmup-hourly': {
+            'task': 'cache-warmup',
+            'schedule': crontab(minute='*/30', hour='*'),
+            'kwargs': {
+                'strategy_name': 'top_n_dashboards',
+                'top_n': 10,
+                'since': '7 days ago',
+            },
         },
     }
 
 
 CELERY_CONFIG = CeleryConfig
 
+WEBDRIVER_TYPE = "chrome"
 FEATURE_FLAGS = {"ALERT_REPORTS": True}
 ALERT_REPORTS_NOTIFICATION_DRY_RUN = True
 WEBDRIVER_BASEURL = "http://superset:8088/"
@@ -112,3 +153,20 @@ try:
     )
 except ImportError:
     logger.info("Using default Docker config...")
+
+CUSTOM_SECURITY_MANAGER = CDASecurityManager
+
+AUTH_TYPE = AUTH_LDAP
+AUTH_ROLE_ADMIN = 'Admin'
+AUTH_USER_REGISTRATION = True
+AUTH_USER_REGISTRATION_ROLE = "Public"
+AUTH_LDAP_GROUP_FIELD = "memberOf"
+
+AUTH_ROLES_MAPPING = {
+    get_env_variable("AUTH_ROLES_MAPPING"): ["Admin"]
+}
+AUTH_LDAP_SERVER = "ldap://{}/".format(get_env_variable("AUTH_LDAP_SERVER_IP"))
+AUTH_LDAP_SEARCH = get_env_variable("AUTH_LDAP_SEARCH")
+AUTH_LDAP_UID_FIELD = get_env_variable("AUTH_LDAP_UID_FIELD")
+AUTH_LDAP_BIND_USER = get_env_variable("AUTH_LDAP_BIND_USER")
+AUTH_LDAP_BIND_PASSWORD = get_env_variable("AUTH_LDAP_BIND_PASSWORD")
